a8dc06eb4179040d878201f88eadcbb9d664c7ce
Lester Hurtado <lester.hurtado@Lesters-MacBook-Pro.local>
2020-11-27 17:34:49 +0800

    new app.json for Laravel

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', env('HEROKU_APP_NAME') ? 'https://' . env('HEROKU_APP_NAME') . '.herokuapp.com' : 'http://localhost'),
 
     'asset_url' => env('ASSET_URL', null),
 
@@ -232,4 +232,5 @@ return [
 
     ],
 
+    'key' => strpos(env('APP_KEY'), 'base64:') !== false ? env('APP_KEY') : substr(env('APP_KEY'), 0, 32),
 ];
